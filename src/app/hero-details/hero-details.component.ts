fb62751f5ac6ab0eae7e0976d19b91d8bf341871
Yehuda Lehrfield <yll2113@columbia.edu>
2023-02-26 22:42:29 -0500

    add details component, property binding with @Input

---
+++ src/app/hero-details/hero-details.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-details',
+  templateUrl: './hero-details.component.html',
+  styleUrls: ['./hero-details.component.css'],
+})
+export class HeroDetailsComponent {
+  @Input() hero?: Hero;
+}
